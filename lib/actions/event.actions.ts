944f119ded94e3da65972fcf655c7c578528de81
Ahmadov zaatreh <samorai-z@hotmail.com>
2025-11-11 14:22:21 +0300

    implement api routes, server actions and caching, and the event details page

---
+++ lib/actions/event.actions.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import { Event, IEvent } from "@/database";
+import dbConnect from "../mongodb";
+
+export const getSimilarEventsBySlug = async (slug: string) => {
+  try {
+    await dbConnect();
+    const event = await Event.findOne({ slug });
+    
+    return await Event.find({ _id: { $ne: event._id }, tags: { $in: event.tags } }).lean<IEvent[]>();
+  } catch {
+    return [];
+  }
+};
