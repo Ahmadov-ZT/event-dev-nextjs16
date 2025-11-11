9dc5c8d4f104a8d55c87998af3992351c668c62e
Ahmadov zaatreh <samorai-z@hotmail.com>
2025-11-11 19:31:29 +0300

    finalize the app

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -2,6 +2,7 @@ import BookEvent from "@/components/BookEvent";
 import EventCard from "@/components/EventCard";
 import { IEvent } from "@/database";
 import { getSimilarEventsBySlug } from "@/lib/actions/event.actions";
+import { cacheLife } from "next/cache";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
@@ -48,10 +49,17 @@ const EventDetailsPage = async ({
 }: {
   params: Promise<{ slug: string }>;
 }) => {
+    'use cache'
+    cacheLife('hours');
   const { slug } = await params;
+  
+
   const request = await fetch(`${BASE_URL}/api/events/${slug}`);
+  const { event } = await request.json();
+  if (!event?.description) return notFound();
+  
   const {
-    event: {
+    
       description,
       image,
       overview,
@@ -63,8 +71,8 @@ const EventDetailsPage = async ({
       audience,
       tags,
       organizer,
-    },
-  } = await request.json();
+    
+  } = event;
 
   if (!description) return notFound();
 
@@ -131,7 +139,7 @@ const EventDetailsPage = async ({
               <p className="text-sm">Be the first to book your spot</p>
             )}
 
-            <BookEvent />
+            <BookEvent eventId={event._id} slug={event.slug} />
           </div>
         </aside>
       </div>
