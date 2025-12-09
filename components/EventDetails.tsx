ef069be91d82631acc68ea31fe38fb09fd79881c
Ahmadov zaatreh <samorai-z@hotmail.com>
2025-12-09 14:14:52 +0300

    Last test for Coderabbit editing

--- components/EventDetails.tsx
+++ components/EventDetails.tsx
@@ -9,6 +9,9 @@ import { cacheLife } from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
+if (!BASE_URL) {
+  throw new Error("NEXT_PUBLIC_BASE_URL environment variable is not set");
+}
 
 const EventDetailItem = ({
   icon,
